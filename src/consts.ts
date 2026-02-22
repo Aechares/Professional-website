635cc29a636c641b55b92d1296379d3d1e310b61
Aechares <99694102+Aechares@users.noreply.github.com>
2026-02-22 00:38:46 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Henry Smith";
-export const SITE_DESCRIPTION = "Henry Smith, MIT S.B. '25 S.M. '27, mechanical engineer";
+export const SITE_TITLE = "Henry R. Smith | Engineering Portfolio";
+export const SITE_DESCRIPTION = "Mechanical Engineering Portfolio - MIT MechE S.B. '25 S.M. '27"
